30637474818fc8776a9d9ab5a037b2579efb1cc8
Hamed <hamed.parvardegari@gmail.com>
2025-11-22 18:17:52 +0330

    Change

--- build.gradle.kts
+++ build.gradle.kts
@@ -50,13 +50,12 @@ android {
 }
 
 dependencies {
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.material)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
-
+    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("com.google.android.material:material:1.11.0")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     implementation("androidx.compose.ui:ui:1.9.5")
     implementation("io.coil-kt:coil-compose:2.7.0")
 }
